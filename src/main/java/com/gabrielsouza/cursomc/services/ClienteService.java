a2b48993657118ffa48e8b312be8fb30266c1fc4
Gabriel Souza <gabrielogf@gmail.com>
2022-04-10 18:32:20 -0300

    Ajustando e testando a delecao de cliente

--- src/main/java/com/gabrielsouza/cursomc/services/ClienteService.java
+++ src/main/java/com/gabrielsouza/cursomc/services/ClienteService.java
@@ -71,7 +71,7 @@ public class ClienteService {
 			
 		}catch (DataIntegrityViolationException e){
 			
-			throw new DataIntegrityException("Não é possivel exlcuir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possivel exlcuir porque há pedidos relacionados");
 			
 		}
 		
