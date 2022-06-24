8bba6275d68fcd7e0b601c74c2ba9daf046e3c59
Gabriel Souza <gabrielogf@gmail.com>
2022-06-23 22:29:11 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/gabrielsouza/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/gabrielsouza/cursomc/services/AbstractEmailService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
