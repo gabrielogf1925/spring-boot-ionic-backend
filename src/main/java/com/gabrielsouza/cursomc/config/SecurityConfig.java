a88a78a9c977e3ad1685aee54056b43bf146ee4e
Gabriel Souza <gabrielogf@gmail.com>
2022-07-18 21:15:24 -0300

    Implementando autorização

--- src/main/java/com/gabrielsouza/cursomc/config/SecurityConfig.java
+++ src/main/java/com/gabrielsouza/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gabrielsouza.cursomc.security.JWTAuthenticationFilter;
+import com.gabrielsouza.cursomc.security.JWTAuthorizationFilter;
 import com.gabrielsouza.cursomc.security.JWTUtil;
 @Configuration
 @EnableWebSecurity
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
