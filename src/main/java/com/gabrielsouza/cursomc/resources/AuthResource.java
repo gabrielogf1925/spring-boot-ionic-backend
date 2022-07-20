c13a0369b7a95266bdcecc15ab6da21a60ba45ca
Gabriel Souza <gabrielogf@gmail.com>
2022-07-19 23:21:40 -0300

    Refresh token

---
+++ src/main/java/com/gabrielsouza/cursomc/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.gabrielsouza.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrielsouza.cursomc.security.JWTUtil;
+import com.gabrielsouza.cursomc.security.UserSS;
+import com.gabrielsouza.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+	
+}
